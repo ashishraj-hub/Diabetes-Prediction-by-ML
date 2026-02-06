fe33dca06ef3729170ae624a92127f150d6015b3
Ashish Raj <ashishraj1601@gmail.com>
2026-02-06 19:23:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,14 +23,22 @@ st.info(
     "Predictions are reliable only for realistic inputs."
 )
 
-preg=st.number_input("Pregnancies",0,20)
-glucose = st.number_input("Glucose", 50, 200)
-bp = st.number_input("Blood Pressure", 40, 120)
+preg = st.number_input("Pregnancies",0,20)
+glucose= st.number_input("Glucose", min_value=50, max_value=300, value=85)
+bp  = st.number_input("Blood Pressure", min_value=40, max_value=200, value=70)
 skin = st.number_input("Skin Thickness", 0, 100)
-insulin = st.number_input("Insulin", 0, 900)
-bmi = st.number_input("BMI", 10.0, 60.0)
+insulin = st.number_input("Insulin", min_value=15, max_value=900, value=120)
+bmi = st.number_input("BMI", min_value=15.0, max_value=70.0, value=25.0)
 dpf = st.number_input("Diabetes Pedigree Function", 0.00, 3.00)
 age = st.number_input("Age", 1, 120)
+
+def clean_input(df):
+    zero_as_missing = [
+        "Glucose", "BloodPressure", "SkinThickness",
+        "Insulin", "BMI"
+    ]
+    df[zero_as_missing] = df[zero_as_missing].replace(0, np.nan)
+    return df
 
 input_df = pd.DataFrame([[preg, glucose, bp, skin, insulin, bmi, dpf, age]],
                         columns=FEATURES)
@@ -39,6 +47,8 @@ st.write("Input Data:",input_df)
 if glucose == 0 or bmi == 0 or bp == 0:
     st.warning("Please enter valid medical values. Zero is not a realistic input.")
     st.stop()
+
+input_df = clean_input(input_df)
 
 st.write("Prediction probability:", pipeline.predict_proba(input_df))
 prob = pipeline.predict_proba(input_df)[0][1]
@@ -52,6 +62,8 @@ if st.button("Prediction"):
         st.success(f"✅ Low risk of diabetes (Probability: {prob:.2f})")
 
 
+st.write("Probability:", prob)
+st.write("Input summary:", input_df.describe())
 
 
 
